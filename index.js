1ec1332913f990ea8990c6279ca5e99381b7723f
Taylor Ryan <taylor.ryan@trifecta.com>
2016-01-12 09:31:46 -0500

    Remove mysql integration.

--- index.js
+++ index.js
@@ -2,13 +2,11 @@
 
 var config = require('./package.json');
 var fs = require('fs');
-var prompt = require('prompt');
 var program = require('commander');
 var colors = require('colors');
 var Promise = require('bluebird');
 var Logger = require('./lib/logger');
 var Hangouts = require('./lib/hangouts');
-var mysql = require('./lib/mysql');
 var sqlite = require('./lib/sqlite');
 var Graph = require('./lib/graph');
 var hangoutParser = Hangouts.parser;
@@ -29,12 +27,7 @@ var promptSchema = {
 program
   .version(config.version)
   .usage('[options] <file>')
-  .option('-u, --username <username>', 'Database username')
-  .option('-p, --password <password>', 'Database password')
-  .option('-h, --host [host]', 'Database host [localhost]', 'localhost')
-  .option('--mysql [database]', 'Send to MySQL database')
   .option('--sqlite [database]', 'Sqlite database','hangouts')
-  .option('-g, --graph', 'Graph Mode')
   .option('-o, --output [file]', 'Output to file', 'output.json')
   .parse(process.argv);
 
@@ -43,84 +36,21 @@ if (program.args.length != 1) {
 }
 
 var conversations = hangoutParser.parse(program.args[0]);
-var dbLogin = {};
 
-if (program.sqlite) {
-  sqlite.sendConversations(conversations);
+sqlite.sendConversations(conversations);
 
-  sqlite.loadMessages()
-  .then(function(messages) {
-    var compiled = Graph.compile(messages);
-    fs.writeFileSync('output.json',JSON.stringify(compiled, null, 2));
-    fs.writeFileSync('web/json/output.json',JSON.stringify(compiled, null, 2));
-    Logger.info('Wrote compiled to output.json');
+sqlite.loadMessages()
+.then(function(messages) {
+  var compiled = Graph.compile(messages);
+  fs.writeFileSync(program.output,JSON.stringify(compiled, null, 2));
+  fs.writeFileSync('web/json/output.json',JSON.stringify(compiled, null, 2));
+  Logger.info('Wrote compiled to '+program.output);
 
-    // Graph.generateLineGraph(compiled, 'month');
-    Graph.generateC3LineGraph(compiled, 'month');
-    // Graph.generateLineGraph(compiled);
-    Graph.generateC3LineGraph(compiled);
-    // Graph.generatePieGraph(compiled);
+  // Graph.generateLineGraph(compiled, 'month');
+  Graph.generateC3LineGraph(compiled, 'month');
+  // Graph.generateLineGraph(compiled);
+  Graph.generateC3LineGraph(compiled);
+  // Graph.generatePieGraph(compiled);
 
-    return 1;
-  }).then(function() {
-    process.exit(0);
-  });
-  return;
-}
-
-if (program.graph) {
-  dbLogin.user = program.username || '';
-  dbLogin.password = program.password || '';
-  dbLogin.database = (typeof program.mysql) === 'string' ? program.mysql : 'hangouts_dev';
-  dbLogin.host = program.host;
-
-  mysql.loadMessages(dbLogin)
-  .then(function(messages) {
-    var compiled = Graph.compile(messages);
-    fs.writeFileSync('output.json',JSON.stringify(compiled, null, 2));
-    fs.writeFileSync('web/json/output.json',JSON.stringify(compiled, null, 2));
-    Logger.info('Wrote compiled to output.json');
-
-    // Graph.generateLineGraph(compiled, 'month');
-    Graph.generateC3LineGraph(compiled, 'month');
-    // Graph.generateLineGraph(compiled);
-    Graph.generateC3LineGraph(compiled);
-    // Graph.generatePieGraph(compiled);
-
-    return 1;
-  }).then(function() {
-    process.exit(0);
-  });
-}
-else if (program.mysql) {
-  dbLogin.user = program.username || '';
-  dbLogin.password = program.password || '';
-  dbLogin.database = (typeof program.mysql) === 'string' ? program.mysql : 'hangouts_dev';
-  dbLogin.host = program.host;
-
-  if (dbLogin.user === '' || dbLogin.password === '') {
-    Logger.warn('Database username and password are required.');
-    prompt.start();
-    prompt.get(promptSchema, function (err, result) {
-      dbLogin.user = result.username;
-      dbLogin.password = result.password;
-
-      mysql.sendConversations(dbLogin, conversations, callback);
-    });
-  }
-  else {
-    mysql.sendConversations(dbLogin, conversations).then(function(){
-      Logger.info('Saved conversations to database.');
-    }, function(err) {
-      console.error(err);
-      Logger.error(err);
-      process.exit(1);
-    });
-  }
-}
-else {
-  // Output to file
-  fs.writeFileSync(program.output,JSON.stringify(conversations, null, 2));
-  Logger.info('Wrote conversations to '+program.output);
-  process.exit(0);
-}
+  return 1;
+});
